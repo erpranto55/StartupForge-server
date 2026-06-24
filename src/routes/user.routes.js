43a277871227e7779e62253f647d962cf1c8ccf1
erpranto <erpranto55@gmail.com>
2026-06-25 01:13:48 +0600

    Updated the API's of User

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,21 +1,19 @@
 import express from "express";
 import { db } from "../config/db.js";
+import { ObjectId } from "mongodb";
 
 const router = express.Router();
 
-const usersCollection =
-  db.collection("users");
-
+const usersCollection = db.collection("users");
 
 // Create User
 router.post("/", async (req, res) => {
   try {
     const user = req.body;
 
-    const existingUser =
-      await usersCollection.findOne({
-        email: user.email,
-      });
+    const existingUser = await usersCollection.findOne({
+      email: user.email,
+    });
 
     if (existingUser) {
       return res.send({
@@ -23,12 +21,11 @@ router.post("/", async (req, res) => {
       });
     }
 
-    const result =
-      await usersCollection.insertOne({
-        ...user,
-        isBlocked: false,
-        createdAt: new Date(),
-      });
+    const result = await usersCollection.insertOne({
+      ...user,
+      isBlocked: false,
+      createdAt: new Date(),
+    });
 
     res.send(result);
   } catch (error) {
@@ -36,14 +33,10 @@ router.post("/", async (req, res) => {
   }
 });
 
-
 // Get All Users
 router.get("/", async (req, res) => {
   try {
-    const users =
-      await usersCollection
-        .find()
-        .toArray();
+    const users = await usersCollection.find().toArray();
 
     res.send(users);
   } catch (error) {
@@ -51,25 +44,84 @@ router.get("/", async (req, res) => {
   }
 });
 
-
 // Get User By Email
-router.get(
-  "/:email",
-  async (req, res) => {
-    try {
-      const email =
-        req.params.email;
+router.get("/:email", async (req, res) => {
+  try {
+    const email = req.params.email;
 
-      const user =
-        await usersCollection.findOne({
-          email,
-        });
+    const user = await usersCollection.findOne({
+      email,
+    });
 
-      res.send(user);
-    } catch (error) {
-      res.status(500).send(error);
-    }
+    res.send(user);
+  } catch (error) {
+    res.status(500).send(error);
   }
-);
+});
+
+router.patch("/block/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const result = await usersCollection.updateOne(
+      {
+        _id: new ObjectId(id),
+      },
+      {
+        $set: {
+          isBlocked: true,
+        },
+      },
+    );
+
+    res.send(result);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+router.patch("/unblock/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const result = await usersCollection.updateOne(
+      {
+        _id: new ObjectId(id),
+      },
+      {
+        $set: {
+          isBlocked: false,
+        },
+      },
+    );
+
+    res.send(result);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+router.patch("/role/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const { role } = req.body;
+
+    const result = await usersCollection.updateOne(
+      {
+        _id: new ObjectId(id),
+      },
+      {
+        $set: {
+          role,
+        },
+      },
+    );
+
+    res.send(result);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
 export default router;
